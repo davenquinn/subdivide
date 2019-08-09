425a391dd9ea73c38c792b79514ee1062e7689d1
Daven Quinn <dev@davenquinn.com>
2019-08-09 15:44:31 -0700

    Allow subdivision of MultiLineStrings

--- subdivide/__init__.py
+++ subdivide/__init__.py
@@ -1,25 +1,28 @@
 """
 Fiona- and geojson-compatible module for interpolating coordinates along lines
 """
-from shapely.geometry import shape, mapping
+from shapely.geometry import shape, mapping, MultiLineString
 import logging as log
 from .strategies import preserve_shape
 from .cut import cut
 
-def subdivide(geometry, interval=1, strategy=preserve_shape):
-    """
-    Subdivides line at a given interval
-    """
-    geometry = shape(geometry)
-    assert geometry.geom_type == "LineString"
+def __subdivide(geometry, interval=1, strategy=preserve_shape):
     coords = [(p.x, p.y) for p in strategy(geometry,interval)]
-
     try:
         geometry.coords = coords
     except ValueError:
         log.info("Could not subdivide: coordinates likely too closely spaced")
 
     return geometry
+
+def subdivide(geometry, **kwargs):
+    """
+    Subdivides line at a given interval
+    """
+    geometry = shape(geometry)
+    if hasattr(geometry,'geoms'):
+        return MultiLineString([__subdivide(g, **kwargs) for g in geometry.geoms])
+    return __subdivide(geometry, **kwargs)
 
 def subdivide_all(records, interval=1, strategy=preserve_shape):
     """
