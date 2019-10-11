849a55c85bbf400eaae80b6f119c7dcc89a645b7
Daven Quinn <dev@davenquinn.com>
2019-10-11 02:54:44 -0500

    Updated setup with more info

--- setup.py
+++ setup.py
@@ -8,10 +8,21 @@ install_requires = [
 
 setup(
     name='subdivide',
-    version="1.0",
+    version="1.0.1",
     description="Subdivides lines by several methods.",
     license='MIT',
+    author='Daven Quinn',
+    author_email='code@davenquinn.com',
+    maintainer='Daven Quinn',
+    maintainer_email='code@davenquinn.com',
+    url='https://github.com/davenquinn/subdivide',
     install_requires=install_requires,
     packages=find_packages(),
     package_dir={'subdivide':'subdivide'},
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: GIS',
+    ],
 )
