fe9d8f3314d1e637eae50d960dd5eb211d00c499
Daven Quinn <dev@davenquinn.com>
2021-11-22 12:53:44 -0600

    Fixed bug in subdivide strategies

--- subdivide/__init__.py
+++ subdivide/__init__.py
@@ -2,9 +2,11 @@
 Fiona- and geojson-compatible module for interpolating coordinates along lines
 """
 from shapely.geometry import shape, mapping
+from shapely.geometry import LineString
 import logging as log
 from .strategies import preserve_shape
 from .cut import cut
+
 
 def subdivide(geometry, interval=1, strategy=preserve_shape):
     """
@@ -12,14 +14,14 @@ def subdivide(geometry, interval=1, strategy=preserve_shape):
     """
     geometry = shape(geometry)
     assert geometry.geom_type == "LineString"
-    coords = [(p.x, p.y) for p in strategy(geometry,interval)]
 
     try:
-        geometry.coords = coords
+        coords = [(p.x, p.y) for p in strategy(geometry, interval)]
+        return LineString(coords)
     except ValueError:
         log.info("Could not subdivide: coordinates likely too closely spaced")
-
     return geometry
+
 
 def subdivide_all(records, interval=1, strategy=preserve_shape):
     """
@@ -27,9 +29,9 @@ def subdivide_all(records, interval=1, strategy=preserve_shape):
     """
     for rec in records:
         try:
-            assert rec['geometry']['type'] == "LineString"
+            assert rec["geometry"]["type"] == "LineString"
             geom = shape(rec["geometry"])
-            coords = [(p.x, p.y) for p in strategy(geom,interval)]
+            coords = [(p.x, p.y) for p in strategy(geom, interval)]
             rec["geometry"]["coordinates"] = coords
 
         except Exception as e:
